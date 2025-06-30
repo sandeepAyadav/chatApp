630562d75243c1e3490e0f4a8027d0e1712144ec
sandeepAyadav <sandeepashokyadav793@gmail.com>
2025-06-29 21:17:40 -0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 import { Pap } from './Pap.jsx';
-export const serverUrl = 'http://localhost:8000';
+export const serverUrl = "https://chatapp-backend-yzxc.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
